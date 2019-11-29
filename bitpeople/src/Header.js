f26dca22a7fdbec2f19bdf5765ea2c0b0f2b7f60
BIT Student <classroom@bgit.rs>
2019-11-29 10:19:37 +0100

    start

---
+++ bitpeople/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>React users</h1>
+        </div>
+    )
+}
+
+export default Header;
